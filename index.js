a1bc79487c94678596fb22544a60a660bd67fcfd
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-06-29 18:14:01 -0400

    commit tree 84f4105bbb8cf84dcc83e49ec8d5a2aa9d7a4235

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+//superbowlWin = (record) => {
+    //const result = record.find( record => record.result === "W");
+    //return result ? result.year : undefined;
+//}
+function superbowlWin(record){
+    const result = record.find(record => record.result === "W");
+    return result ? result.year : undefined;
+}
